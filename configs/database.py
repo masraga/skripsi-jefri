e8806562d26cfb58952a6505c2f3bcb04a4be5c2
masraga <github.ragamulia@gmail.com>
2024-12-05 14:09:39 +0700

    feat: set default admin

--- configs/database.py
+++ configs/database.py
@@ -1,4 +1,7 @@
-import mysql.connector
+import os, mysql.connector, hashlib
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 class database:
   host=""
@@ -7,12 +10,15 @@ class database:
 
   password=""
 
+  name=""
+
   instance=None
 
-  def __init__(self, host, user, password):
+  def __init__(self, host, user, password, name):
     self.host = host
     self.user = user
     self.password = password
+    self.name = name
   
   def connect(self):
     print("try to connect to mysql"); 
@@ -22,6 +28,74 @@ class database:
         user=self.user,
         password=self.password,
       )
+      return self.instance
       print("mysql connect successfully")
     except mysql.connector.Error as err:
       raise Exception(err)
+  
+  def get_instance(self):
+    return self.instance
+  pass
+
+class migration:
+  db = None
+
+  connector = None
+
+  is_connect_db = False
+
+  def __init__(self, db):
+    self.db = db
+    self.connector = db.cursor()
+    self.check_db()
+    pass
+
+  def check_db(self):
+    return self.connector.execute("CREATE DATABASE IF NOT EXISTS {}".format(os.getenv('DB_NAME')))
+
+  def up(self):
+    self.db.database = os.getenv('DB_NAME')
+    query = {
+      self.user_table()['up']
+    }
+    for q in query: 
+      self.db.cursor().execute(q)
+    pass
+
+  def down(self):
+    self.db.database = os.getenv('DB_NAME')
+    query = {
+      self.user_table()['down']
+    }
+    for q in query:
+      self.db.cursor().execute(q)
+    pass
+
+  def user_table(self):
+    up = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), is_active TINYINT(1))"
+    down = "DROP TABLE IF EXISTS users"
+    return {"up": up, "down": down}
+  pass
+
+class init_data:
+  db = None
+  def __init__(self, db):
+    self.db = db
+    self.db.database = os.getenv('DB_NAME')
+
+  def run(self):
+    self.create_user()
+    pass
+
+  def create_user(self):
+    user_cursor = self.db.cursor(dictionary=True)
+    user_cursor.execute("SELECT * FROM users WHERE username='{}'".format(os.getenv('DB_USER_NAME')))
+    user_cursor = user_cursor.fetchone()
+    self.db.commit()
+    if user_cursor == None:
+      user_cursor = self.db.cursor()
+      query = "INSERT INTO users (username, password, is_active) VALUES (%s, %s, %s)"
+      query_value = (os.getenv('DB_USER_NAME'), hashlib.sha1(os.getenv('DB_USER_PASSWORD').encode()).hexdigest(), 1)
+      user_cursor.execute(query, query_value)
+      self.db.commit()
+    pass
--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import os, secrets
-from flask import Flask, request, render_template, session
+from flask import Flask
+from dotenv import load_dotenv
 
-from configs.database import database
+from configs.database import database, migration, init_data
 
 from controllers.user_controller import user_controller
 
+#load env
+load_dotenv()
+
 # define database
-db = database("localhost", "root", "")
-db.connect()
+db = database(
+  os.getenv('DB_HOST'), 
+  os.getenv('DB_USER'), 
+  os.getenv('DB_PASSWORD'), 
+  os.getenv('DB_NAME')
+)
+
+#run migration
+if(os.getenv('DB_MIGRATE')):
+  new_migration = migration(db.connect())
+  new_init_data = init_data(db.connect())
+  # new_migration.up() if os.getenv('DB_MIGRATE_TYPE') == "up" else new_migration.down()
+  if os.getenv('DB_MIGRATE_TYPE') == "up":
+    new_migration.up()
+    new_init_data.run()
+  else:
+    new_migration.down()
 
 #define controller
 new_user_controller = user_controller(db)
